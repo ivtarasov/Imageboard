6ff023e3b6ae06142ab8c762fec09717261ac344
vvv3ww2 <iiiiiiiiiii11@protonmail.com>
2021-01-20 19:04:06 +0700

    Fix WakabaMark impl.

--- Imageboard.WakabaMark/WakabaMark.cs
+++ Imageboard.WakabaMark/WakabaMark.cs
@@ -11,33 +11,25 @@ namespace Imageboard.WakabaMark
             char[] value = sourse.ToCharArray();
             MarkUp(ref value, marksDict);
         }
-        private static void MarkUp(ref char[]  value, Dictionary<Mark, int> marksDict)
+        private static void MarkUp(ref char[] value, Dictionary<Mark, int> marksDict)
         {
-            for (int i = 0; i < value.Length; i++)
+            var i = 0;
+            while (i < value.Length)
             {
                 var mappedValue = CharToMarkMapper.Map(value[i]);
-                switch (mappedValue)
+                if (mappedValue != Mark.None)
                 {
-                    case Mark.Code:
-                        if (marksDict.ContainsKey(mappedValue))
-                        {
-                            value[marksDict[mappedValue]] = MarkToHtmlMapper.Map(mappedValue);
-                            value[i] = MarkToHtmlMapper.SecondMap(mappedValue);
-                            MarkUp(ref value, marksDict);
-                        }
-                        marksDict.Add(mappedValue, i);
+                    if (marksDict.ContainsKey(mappedValue))
+                    {
+                        value[marksDict[mappedValue]] = MarkToHtmlMapper.Map(mappedValue);
+                        value[i] = MarkToHtmlMapper.SecondMap(mappedValue);
                         MarkUp(ref value, marksDict);
-                        return;
-                    case Mark.Bold:
-                        return;
-                    case Mark.Italic:
-                        return;
-                    case Mark.Spoler:
-                        return;
-                    default:
-                        return;
+                    }
+                    marksDict.Add(mappedValue, i);
+                    MarkUp(ref value, marksDict);
+                    return;
                 }
-
+                i++;
             }
         }
         static private void ChangeChars(ref char value)
