6b22e2a9d88b05f6cf66ea969d36f014b7493936
vvv3ww2 <iiiiiiiiiii11@protonmail.com>
2021-02-03 04:14:14 +0700

    Imageboard.data: add a factory for the applicationdbcontext

--- Imageboard.Data/Contexts/ApplicationDbContext.cs
+++ Imageboard.Data/Contexts/ApplicationDbContext.cs
@@ -1,8 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Imageboard.Data.Enteties;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace Imageboard.Data.Contexts
 {
+    public class ApplicationDbContextFactory
+    {
+        public static ApplicationDbContext CreateDbContext()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=mobilesdb;Trusted_Connection=True;"); // from config file in future
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Board> Boards { get; set; }
