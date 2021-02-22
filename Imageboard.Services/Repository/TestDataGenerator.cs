31fdf2d7f593349657f48c95a1a00a927ea4d496
vvv3ww2 <iiiiiiiiiii11@protonmail.com>
2021-02-22 21:15:43 +0700

    Move TestDataGenerator to Imageboard.Services.Repository

--- Imageboard.Services/Repository/TestDataGenerator.cs
+++ Imageboard.Services/Repository/TestDataGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 
-namespace Imageboard.Tests
+namespace Imageboard.Services.Repository
 {
     public class TestDataGenerator
     {
