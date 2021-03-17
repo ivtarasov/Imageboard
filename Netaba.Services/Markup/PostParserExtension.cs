f26e0aac3845f22f1ae710e1938655b7f410d5e9
vvv3ww2 <iiiiiiiiiii11@protonmail.com>
2021-03-17 16:36:58 +0700

    Add PostParserExtensions

---
+++ Netaba.Services/Markup/PostParserExtension.cs
@@ -0,0 +1,11 @@
+﻿using Netaba.Data.Models;
+using System.Threading.Tasks;
+
+namespace Netaba.Services.Markup
+{
+    public static class PostParserExtension
+    {
+        public static async Task ParseMessageAsync(this Post post, IParser parser, string boardName) => 
+            post.Message = await parser.ToHtmlAsync(post.Message, boardName);
+    }
+}
