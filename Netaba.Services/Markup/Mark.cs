e99d4d8045614b4c1f960c542af97f1428362c52
vvv3ww2 <iiiiiiiiiii11@protonmail.com>
2021-04-07 14:54:46 +0700

    Add few comments

--- Netaba.Services/Markup/Mark.cs
+++ Netaba.Services/Markup/Mark.cs
@@ -3,17 +3,20 @@ namespace Netaba.Services.Markup
 {
     public enum Mark
     {
+        // Сan be everywhere 
         Monospace,
         Bold,
         Italic,
         Spoler,
 
+        // Marks that must be at the beginning of a line to be recognized 
         Link,
         OList,
         UnList,
         ListElem,
         Quote,
 
+        // Mark that is used only for parsing
         NewLine,
         Edge,
         None
